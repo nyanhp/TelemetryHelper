9c5ead84a6cf87a1b1659664f7b161f3e1572857
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2019-09-14 11:51:11 +0200

    Meh

---
+++ TelemetryHelper/internal/scripts/loadLibs.ps1
@@ -0,0 +1,22 @@
+$typeLoaded = try
+{    
+    [de.janhendrikpeters.TelemetryHelper]
+    Write-PSFMessage -Message 'Type imported'
+}
+catch
+{
+    Write-PSFMessage -Message 'Type not imported yet'
+    $false
+}
+
+if (-not $typeLoaded -and $PSVersionTable.PSEdition -eq 'Core')
+{
+    Write-PSFMessage -Message "Loading core type from $($script:ModuleRoot)\bin\core\TelemetryHelper.dll"
+    Add-Type -Path "$($script:ModuleRoot)\bin\core\TelemetryHelper.dll"
+}
+
+if (-not $typeLoaded -and $PSVersionTable.PSEdition -eq 'Desktop')
+{
+    Write-PSFMessage -Message "Loading full type from $($script:ModuleRoot)\bin\core\TelemetryHelper.dll"
+    Add-Type -Path "$($script:ModuleRoot)\bin\full\TelemetryHelper.dll"
+}
