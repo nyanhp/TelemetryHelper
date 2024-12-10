747a116f0f83293b870d405378ff8202b0299d08
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2024-12-10 11:06:40 +0100

    Do not throw on import

--- TelemetryHelper/internal/scripts/loadLibs.ps1
+++ TelemetryHelper/internal/scripts/loadLibs.ps1
@@ -1,5 +1,5 @@
 ﻿try {
     Add-Type -Path "$script:moduleRoot/bin/netstandard2.0/TelemetryHelper.dll" -ErrorAction Stop
 } catch {
-    Write-Error -Message "Unable to import telemetry library."
+    Write-PSFMessage -Message "Unable to import telemetry library."
 }
